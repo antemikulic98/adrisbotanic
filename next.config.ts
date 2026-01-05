bf4d6698e66db64c85fcf013dafcd4a0478b4ad9
Ante Mikulic <mikulicante98@gmail.com>
2026-01-05 20:53:45 +0100

    novo

--- next.config.ts
+++ next.config.ts
@@ -7,9 +7,6 @@ const nextConfig: NextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 31536000, // 1 year cache
   },
-  experimental: {
-    optimizeCss: true,
-  },
 };
 
 export default nextConfig;
